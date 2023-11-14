0985b99e8d670337419f8096e840121da965ac73
Sulaimon <abodunrinsulaimon1@gmail.com>
2023-11-14 16:58:36 +0100

    Simple Modification

--- Blog_Generator/models.py
+++ Blog_Generator/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Blog(models.Model):
     """ The User Blog Model
         This is to save the blog post of each user
